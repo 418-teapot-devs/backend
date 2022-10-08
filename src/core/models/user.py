bf125045bc4b95adeba9050ab96b7da7ef9f48b8
Bruno Castellano <bruno.castellano@mi.unc.edu.ar>
2022-10-08 16:53:47 -0300

    Rename attributes

--- src/core/models/user.py
+++ src/core/models/user.py
@@ -2,10 +2,10 @@ from pony.orm import PrimaryKey, Required, Set
 
 from src.core.models.database import db
 
-
 class User(db.Entity):
-    username = PrimaryKey(str, 32)
+    name = PrimaryKey(str, 32)
+    e_mail = Required(str, unique=True)
     password = Required(str)
     has_avatar = Required(bool, default=False)
-    e_mail = Required(str)
     is_verified = Required(bool, default=False)
+
