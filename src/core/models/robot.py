c4d8b5834f746f640ad10b288bb43bb20d6e7e1d
Bruno Castellano <bruno.castellano@mi.unc.edu.ar>
2022-10-09 17:16:48 -0300

    Added explanatory comments about avatar storage

--- src/core/models/robot.py
+++ src/core/models/robot.py
@@ -6,6 +6,8 @@ from core.models.database import db
 class Robot(db.Entity):
     owner = Required("User")
     name = Required(str, 32)
+    # if robot has avatar, it is stord in /assets/avatars/robots/{owner.name}_{name}.png
+    # otherwise, it uses /assets/defaults/robot_avatar.png
     has_avatar = Required(bool, default=False)
     played_matches = Required(int, default=0)
     won_matches = Required(int, default=0)
