d660480db110fa2c1c14740b4d93306ba425377a
Alvaro Roy Schachner <alvarolegit@gmail.com>
2022-10-11 18:53:19 -0300

    Added register form validation

--- src/core/schemas/user.py
+++ src/core/schemas/user.py
@@ -1,4 +1,17 @@
-from pydantic import BaseModel, EmailStr, HttpUrl
+from password_validator import PasswordValidator
+from pydantic import BaseModel, EmailStr, HttpUrl, validator
+
+pass_validator = (
+    PasswordValidator()
+    .min(8)
+    .max(255)
+    .has()
+    .uppercase()
+    .has()
+    .lowercase()
+    .has()
+    .digits()
+)
 
 
 class Register(BaseModel):
@@ -6,3 +19,10 @@ class Register(BaseModel):
     password: str
     e_mail: EmailStr
     avatar: HttpUrl | None
+
+    @validator("password")
+    def password_is_correct(cls, v):
+        if not pass_validator.validate(v):
+            raise ValueError("value is not a valid password")
+
+        return v
