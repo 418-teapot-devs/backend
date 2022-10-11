6a9c0adf379f96d9dab7a16f40d1c2727be2e08b
leo10t <leo.torres@mi.unc.edu.ar>
2022-10-11 20:21:51 -0300

    Format

--- src/core/models/matches.py
+++ src/core/models/matches.py
@@ -2,11 +2,12 @@ from pony.orm import PrimaryKey, Required, Set
 
 from core.models.database import db
 
+
 class Match(db.Entity):
-    hosts = Required("User",reverse="matches_hosts")
-    name = Required(str,32)
+    hosts = Required("User", reverse="matches_hosts")
+    name = Required(str, 32)
     robot_count = Required(int)
     game_count = Required(int, default=100)
     round_count = Required(int, default=10000)
     plays = Set("Robot", reverse="matches_in")
-    state = Required(str,6)
+    state = Required(str, 6)
