640fa99fe0c8187844dcf2dc021d44e5fc546148
leo10t <leo.torres@mi.unc.edu.ar>
2022-10-11 20:01:42 -0300

    created matches table

---
+++ src/core/models/matches.py
@@ -0,0 +1,12 @@
+from pony.orm import PrimaryKey, Required, Set
+
+from core.models.database import db
+
+class Match(db.Entity):
+    hosts = Required("User",reverse="matches_hosts")
+    name = Required(str,32)
+    robot_count = Required(int)
+    game_count = Required(int, default=100)
+    round_count = Required(int, default=10000)
+    plays = Set("Robot", reverse="matches_in")
+    state = Required(str,6)
