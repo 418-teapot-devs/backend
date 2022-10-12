5f32a84cc24acd314189954a1b331731fca7109e
leo10t <leo.torres@mi.unc.edu.ar>
2022-10-12 10:34:49 -0300

    added test and small modifications in user and match tables

--- src/core/models/matches.py
+++ src/core/models/matches.py
@@ -4,10 +4,10 @@ from core.models.database import db
 
 
 class Match(db.Entity):
-    hosts = Required("User", reverse="matches_hosts")
+    host = Required("User", reverse="matches_hosts")
     name = Required(str, 32)
-    robot_count = Required(int)
+    robot_count = Required(int, default=1)
     game_count = Required(int, default=100)
     round_count = Required(int, default=10000)
     plays = Set("Robot", reverse="matches_in")
-    state = Required(str, 6)
+    state = Required(str, 6,default="Lobby")
