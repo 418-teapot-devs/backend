a50830ed3eb47fe3d5e85c9cd0a1f0f000b4ce0f
Bruno Castellano <bruno.castellano@mi.unc.edu.ar>
2022-10-15 19:30:42 -0300

    Fixed math for movement and added tests for class Robot

--- src/game/board.py
+++ src/game/board.py
@@ -1,8 +1,11 @@
-import inspect, importlib
+import importlib
+import inspect
 from typing import Dict, List, Tuple
-from robot import Robot, MAX_DMG
+
+from robot import MAX_DMG, Robot
 
 Board = List[Robot]
+
 
 def nextRound(board: Board) -> Board:
     for r in board:
@@ -20,7 +23,7 @@ def initBoard(files: List[str]) -> Board:
     for file in files:
         module = importlib.import_module(file)
         classes = inspect.getmembers(module, inspect.isclass)
-        classes = list(filter(lambda c: c[0] != 'Robot', classes))
+        classes = list(filter(lambda c: c[0] != "Robot", classes))
         assert len(classes) == 1
         robotName = classes[0][0]
         # getattr returns a class, which we immediately initialize
@@ -31,14 +34,16 @@ def initBoard(files: List[str]) -> Board:
 
 
 def board2dict(board: Board) -> Dict[int, Tuple[float, float]]:
-    return { r._id: r._pos for r in board }
+    return {r._id: r._pos for r in board}
 
 
-def game2dict(game: List[Dict[int, Tuple[float, float]]]) -> Dict[int, Dict[str, List[float]]]:
-    summary = { r_id: { 'x': [], 'y': [] } for r_id in game[0].keys() }
+def game2dict(
+    game: List[Dict[int, Tuple[float, float]]]
+) -> Dict[int, Dict[str, List[float]]]:
+    summary = {r_id: {"x": [], "y": []} for r_id in game[0].keys()}
     # summary = { r_id: ([r_pos[0]], [r_pos[0]]) for r_id, r_pos in game[0].items() }
     for round in game:
         for r_id, pos in round.items():
-            summary[r_id]['x'].append(pos[0])
-            summary[r_id]['y'].append(pos[1])
+            summary[r_id]["x"].append(pos[0])
+            summary[r_id]["y"].append(pos[1])
     return summary
---
+++ src/game/test_robot.py
@@ -0,0 +1,76 @@
+import pytest
+import math
+
+from .robot import *
+
+
+def test_invalid_bot():
+    class Bad1(Robot):
+        def respond(self):
+            return
+
+    class Bad2(Robot):
+        def initialize(self):
+            return
+
+    with pytest.raises(TypeError):
+        Robot(0, (0, 0))
+    with pytest.raises(TypeError):
+        Bad1(0, (0, 0))
+    with pytest.raises(TypeError):
+        Bad2(0, (0, 0))
+
+class RoboTest(Robot):
+    def initialize(self):
+        self.var = 90
+    def respond(self):
+        self.var /= 2
+        self.drive(self.var, 400)
+        self.var *= 3
+
+def test_init_bot():
+    r = RoboTest(1, (2,3))
+    r.initialize()
+    assert r._id == 1
+    assert r._pos == (2,3)
+    assert r._dir == 0
+    assert r._vel == 0
+    assert r.var == 90
+
+def test_bot_vars():
+    r = RoboTest(1, (2,3))
+    r.initialize()
+    r.respond()
+    assert r._dir == 45
+    assert r._vel == 100
+    assert r.var == 135
+
+def test_bot_move():
+    r = RoboTest(1, (500, 500))
+    r.initialize()
+    r.respond()
+    r._move_and_check_crash([])
+    assert r._pos == (500 + 100 * DELTA_TIME * math.cos(math.radians(45)), 500 + 100 * DELTA_TIME * math.sin(math.radians(45)))
+
+def test_bot_crash():
+    r1 = RoboTest(1, (500, 500))
+    r2 = RoboTest(2, (500, 500))
+    r1.initialize()
+    r2.initialize()
+    r1.respond()
+    r2.respond()
+    r1._move_and_check_crash([r2])
+    assert r1._dmg == r2._dmg == COLLISION_DMG
+
+class NoMove(Robot):
+    def initialize(self):
+        return
+    def respond(self):
+        return
+
+def test_bot_wall():
+    r1 = NoMove(1, (-1,-1))
+    r1.initialize()
+    r1.respond()
+    r1._move_and_check_crash([])
+    assert r1._dmg == COLLISION_DMG
