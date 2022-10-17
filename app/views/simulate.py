88e87b5891bf1db2c7e9c88227bdb5cbf304cc77
Bruno Castellano <bruno.castellano@mi.unc.edu.ar>
2022-10-17 16:12:51 -0300

    Added verification requirement for simulation

--- app/views/simulate.py
+++ app/views/simulate.py
@@ -1,5 +1,8 @@
 from os.path import isfile
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Header
+from pony.orm import db_session
+from core.models.robot import Robot
+from views import get_current_user
 
 from game.board import board2dict, game2dict, initBoard, nextRound
 from core.schemas.simulation import SimulationRequest
@@ -11,11 +14,17 @@ router = APIRouter()
 
 
 @router.post("/")
-def simulate(schema: SimulationRequest):
+def simulate(schema: SimulationRequest, token: str = Header()):
+    username = get_current_user(token)
+
     for bot in schema.robots:
         if not isfile(f"{BOT_DIR}/{bot}.py"):
             raise HTTPException(status_code=404, detail=f"robot {bot} not found")
+        with db_session:
+            if Robot.get(id=bot).owner.name != username:
+                raise HTTPException(status_code=403, detail=f"robot requested is not owned by user {username}")
     rounds = schema.rounds if schema.rounds is not None else DEFAULT_ROUNDS
+
     b = initBoard(list(f".{bot}" for bot in schema.robots))
     g = [board2dict(b)]
     for _ in range(rounds):
