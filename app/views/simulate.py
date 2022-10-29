83adafcd7f18b5d02b91b8c4fcb0363e3defdd48
Bruno Castellano <bruno.castellano@mi.unc.edu.ar>
2022-10-29 17:08:15 -0300

    Fixed schema for simulation

--- app/views/simulate.py
+++ app/views/simulate.py
@@ -5,6 +5,8 @@ from pony.orm import db_session
 
 from app.game.board import Board
 from app.models.robot import Robot
+from app.models.user import User
+from app.schemas.match import RobotInMatch
 from app.schemas.simulation import SimulationRequest, SimulationResponse
 from app.util.auth import get_current_user
 
@@ -18,15 +20,24 @@ router = APIRouter()
 def simulate(schema: SimulationRequest, token: str = Header()):
     username = get_current_user(token)
 
-    for bot in schema.robots:
-        if not isfile(f"{BOT_DIR}/{bot}.py"):
-            raise HTTPException(status_code=404, detail=f"robot {bot} not found")
-        with db_session:
+    robots = {}
+
+    with db_session:
+        cur_user = User.get(username)
+        if cur_user is None:
+            raise HTTPException(status_code=404, detail="User not found")
+
+        for bot in schema.robots:
+            r = Robot.get(id=bot).owner.name != username
+            if r is None:
+                raise HTTPException(status_code=404, detail="Robot not found")
             if Robot.get(id=bot).owner.name != username:
                 raise HTTPException(
                     status_code=403,
                     detail=f"robot requested is not owned by user {username}",
                 )
+            robots[bot] = RobotInMatch(name=r.name, avatar_url=None, username=r.owner)
+
     rounds = schema.rounds if schema.rounds is not None else DEFAULT_ROUNDS
 
     b = Board(schema.robots)
@@ -34,4 +45,5 @@ def simulate(schema: SimulationRequest, token: str = Header()):
     for _ in range(rounds):
         b.next_round()
         g.append(b.to_round_schema())
-    return SimulationResponse(rounds=g)
+
+    return SimulationResponse(robots=robots,rounds=g)
