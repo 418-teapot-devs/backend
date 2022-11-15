56ed1a69741fb47e5a8f1c03c1ea8d76364518c8
Alvaro Roy Schachner <alvarolegit@gmail.com>
2022-11-15 15:07:08 -0300

    Matches now update robot mmrs, added tests

--- tests/views/test_robots.py
+++ tests/views/test_robots.py
@@ -7,7 +7,8 @@ from pony.orm import db_session
 from app.main import app
 from app.models import Robot
 from app.views.users import create_access_token
-from tests.testutil import create_random_robots, register_random_users
+from app.models.robot import Robot
+from tests.testutil import register_random_users, create_random_robots, create_random_matches
 
 cl = TestClient(app)
 
@@ -98,3 +99,40 @@ def test_get_robots():
     for i, r in enumerate(test_robots):
         response = cl.get("/robots/", headers={"token": user[i]["token"]})
         assert response.json() == r
+
+
+def test_robot_results():
+    host, *players = register_random_users(4)
+
+    create_random_robots(host["token"], 33)
+    match = create_random_matches(host["token"], 1)[0]
+
+    for player in players:
+        player["robot"] = create_random_robots(player["token"], 1)[0]
+
+        join_form = {"robot_id": player["robot"]["id"], "password": match["password"]}
+        token_header = {"token": player["token"]}
+
+        response = cl.put(f"/matches/{match['id']}/join/", headers=token_header, json=join_form)
+        assert response.status_code == 201
+
+    host_token_header = {"token": host["token"]}
+    response = cl.put(f"/matches/{match['id']}/start/", headers=host_token_header)
+    assert response.status_code == 201
+
+    match_result = cl.get(f"/matches/{match['id']}/", headers=host_token_header).json()
+    for robot_id in match_result["robots"]:
+        robot_result = match_result["results"][robot_id]
+
+        expected_mmr = 0
+        expected_won_matches = 0
+        if robot_result["robot_pos"] == 1:
+            expected_mmr = 20
+            expected_won_matches = 1
+
+        with db_session:
+            robot = Robot[robot_id]
+
+        assert robot.played_matches == 1
+        assert robot.mmr == expected_mmr
+        assert robot.won_matches == expected_won_matches
