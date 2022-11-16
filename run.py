f905783e2f3ed8ad1e5c3a6124f8c40749214882
Bruno Castellano <bruno.castellano@mi.unc.edu.ar>
2022-11-16 18:02:33 -0300

    Test cleanup, added possibility to run only some tests from `run.py`

--- run.py
+++ run.py
@@ -5,15 +5,21 @@ import coverage
 import pytest
 import uvicorn
 
-test = "tests" in sys.argv
+testpaths = []
+for arg in sys.argv:
+    if arg.startswith("tests"):
+        testpaths.append(arg)
+
+test = len(testpaths) != 0
+
 os.environ["PYROBOTS_DBFILE"] = ":sharedmemory:" if test else "db.sqlite"
 os.environ["PYROBOTS_ASSETS"] = "tests/assets" if test else "app/assets"
 
 if __name__ == "__main__":
     if test:
-        cov = coverage.Coverage(omit=["tests/*", "app/util/ws.py"])
+        cov = coverage.Coverage(omit=["tests/*"])
         cov.start()
-        pytest.main(["-vvx", "tests"])
+        pytest.main(["-vvx"] + testpaths)
         cov.stop()
         cov.save()
         cov.html_report()
